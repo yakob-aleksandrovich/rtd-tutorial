11a6c6d1963dc3419b362f73a02681190826014f
yakob-aleksandrovich <jakobaleksandrovich@gmail.com>
2023-10-03 11:50:51 +0200

    Settings to display Hebrew

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,3 +37,23 @@ epub_show_urls = 'footnote'
 # -- Options for Latex
 #       Use XeLaTex (to properly handle Unicode)
 latex_engine = 'xelatex'
+
+latex_elements = {
+     # The paper size ('letterpaper' or 'a4paper').
+     #
+     # 'papersize': 'letterpaper',
+
+     # The font size ('10pt', '11pt' or '12pt').
+     #
+     # 'pointsize': '10pt',
+
+     'label': '\\usepackage[english]{babel}',
+
+     # Additional stuff for the LaTeX preamble.
+     #
+     'preamble': r'''
+\setmainfont{SBL Hebrew}
+\XeTeXlinebreaklocale "he"
+\XeTeXlinebreakskip = 0pt plus 1pt
+'''
+}
