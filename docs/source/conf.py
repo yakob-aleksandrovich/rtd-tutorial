e8cded8e1ae30415c880c6da28977c88d84e92ad
yakob-aleksandrovich <jakobaleksandrovich@gmail.com>
2023-10-03 11:42:27 +0200

    Enable xelatex

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# -- Options for Latex
+#       Use XeLaTex (to properly handle Unicode)
+latex_engine = 'xelatex'
